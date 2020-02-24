c2318a1fa3becb2ae5918d190a17a0bc22935f9d
Alex Langfield <alexlangfield@csm-wl-dhcp-206-22.mines.edu>
2020-02-23 18:32:20 -0700

    adding IntBoard java class

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,34 @@
 package experiment;
+import java.util.*;
 
 public class IntBoard {
+	private Set<BoardCell> cells = new HashSet<BoardCell>();
+	private Set<BoardCell> targets = new HashSet<BoardCell>();
+ 	private Map<BoardCell, Set<BoardCell>> adjacent = new HashMap<BoardCell, Set<BoardCell>>();
+	
 
-}
+	public IntBoard(Set<BoardCell> cells) {
+		super();
+		this.cells = cells;
+		// Will call calcAdject
+	}
+	public void calcAdjacencies()
+	{
+		// Calcs adjacents and saves to Map adjacent
+	}
+	public Set<BoardCell> getAdjList(BoardCell currentCell)
+	{
+		return null;
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength)
+	{
+		
+	}
+	
+	public Set<BoardCell> getTargets()
+	{
+		return null;
+	}
+	
+}	
