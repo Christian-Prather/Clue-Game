c1675575513ab14a0df16aab14dd111f27510780
Christian Prather <Clemsbach@gmail.com>
2020-02-25 13:34:16 -0700

    Added skeletons for classes

---
+++ src/clueGame/Board.java
@@ -0,0 +1,48 @@
+package clueGame;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Board 
+{
+	private int numRows;
+	private int numColumns;
+	public int MAX_BOARD_SIZE = 50;
+	private Board boardCell[][];
+	private Map<Character, String> legend;
+	private Map<BoardCell, Set<BoardCell>> adjjMatrix;
+	private Set<BoardCell> targets;
+	private String boardConfigFile;
+	private String roomConfigFile;
+	private static Board theInstance = new Board();
+
+	
+	private Board() {}
+	// this method returns the only Board
+	public static Board getInstance() 
+	{
+		return theInstance;
+	}
+	
+	public void initalize()
+	{
+		
+	}
+	public void loadRoomConfig()
+	{
+		
+	}
+	public void loadBoardConfig()
+	{
+		
+	}
+	public void calcAdjacencies()
+	{
+		
+	}
+	public void calcTargets(BoardCell cell, int pathLength)
+	{
+		
+	}
+
+}
---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,31 @@
+package clueGame;
+
+public class BoardCell {
+	private int row;
+	private int column;
+	private Character initial;
+	
+	
+	public BoardCell(int x, int y)
+	{
+		// Flipped for programmer preference
+		this.column = x;
+		this.row = y;
+	}
+	
+	public Boolean isWalkway()
+	{
+		return null;
+	}
+	
+	public Boolean isRoom()
+	{
+		return null;
+	}
+	
+	public Boolean isDoorway()
+	{
+		return null;
+	}
+	
+}
